3559b6eccbd9668fa8d30dc7c23c550d54f98268
Yauri Attamimi <yauritux@gmail.com>
2018-07-14 09:51:06 +0700

    add sample for channel communication (CSP)

---
+++ mini-classes/go/demo-channel/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func calculateSquares(number int, squareop chan int) {
+	sum := 0
+	for number != 0 {
+		digit := number % 10
+		sum += digit * digit
+		number /= 10
+	}
+	squareop <- sum
+}
+
+func calculateCubes(number int, cubeop chan int) {
+	sum := 0
+	for number != 0 {
+		digit := number % 10
+		sum += digit * digit * digit
+		number /= 10
+	}
+	cubeop <- sum
+}
+
+func main() {
+	number := 589
+	sqrch := make(chan int)
+	cubech := make(chan int)
+	go calculateSquares(number, sqrch)
+	go calculateCubes(number, cubech)
+	squares, cubes := <-sqrch, <-cubech
+	fmt.Println("Final Output", squares+cubes)
+}
