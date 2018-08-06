340efb4237423c8cb2be1b8b032323824acefdbf
Yauri Attamimi <yauritux@gmail.com>
2018-08-06 22:30:06 +0700

    update by adding loop

--- workshop/java/microservices-with-springcloud/lab-instructions/jenkins/Hello.java
+++ workshop/java/microservices-with-springcloud/lab-instructions/jenkins/Hello.java
@@ -1,6 +1,8 @@
 public class Hello {
 
 	public static void main(String... args) {
-		System.out.println("Hello Java from Jenkins World...");
+		for (int i = 1; i < 10; i++) {
+			System.out.println("Hello Java from Jenkins World...#" + i);
+		}
 	}
 }
