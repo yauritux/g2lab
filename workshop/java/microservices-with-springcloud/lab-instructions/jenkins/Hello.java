3abae000961ac87c4dd9ae8e8d2af01b361b3ec1
Yauri Attamimi <yauritux@gmail.com>
2018-08-06 22:31:57 +0700

    add label

--- workshop/java/microservices-with-springcloud/lab-instructions/jenkins/Hello.java
+++ workshop/java/microservices-with-springcloud/lab-instructions/jenkins/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
 
 	public static void main(String... args) {
+		System.out.println("This simple app is created to give an oversight about how to use Jenkins with Git integration");
 		for (int i = 1; i < 10; i++) {
 			System.out.println("Hello Java from Jenkins World...#" + i);
 		}
